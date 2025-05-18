4be4c2b887b813c5d334230a700a2054c5f78048
khanriazaoif <khanriazaoif@gmail.com>
2025-05-18 15:19:17 -0500

    Initial commit

---
+++ module_one/python_basics/hello.py
@@ -0,0 +1,12 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+if first_name == "Riaz":
+    print(f"Hello, {first_name} {last_name}!")
+    print("Have a great day {}!".format(first_name))
+elif first_name == "Rizwan":
+    print(f"Hello, {first_name} {last_name}!")
+    print("Have a bad day {}!".format(first_name))
+else:
+    print(f"Hello, {first_name} {last_name}!")
+    print("Have a great day {}!".format(first_name))
